02d698356fc9e556675b06ae7e31fe9a32d595ae
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2020-07-21 20:06:48 +0500

    Added LineGraph

--- src/Components/LineGraph/LineGraph.js
+++ src/Components/LineGraph/LineGraph.js
@@ -1,7 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./LineGraph.css";
+import { Line } from "react-chartjs-2";
+import numeral from "numeral";
 
-function LineGraph() {
-  return <div></div>;
+const options = {
+  legend: { display: false },
+  elements: {
+    points: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (toolTipItem, data) {
+        return numeral(toolTipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
+
+const buildChartData = (data, casesType = "cases") => {
+  const chartData = [];
+  let lastDataPoint;
+
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      //   ...
+      const newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+function LineGraph({ casesType = "cases" }) {
+  const [data, setData] = useState({});
+  //   ! https://disease.sh/v3/covid-19/historical/all?lastdays=120
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          //   render here
+          const chartData = buildChartData(data, "cases");
+          setData(chartData);
+        });
+    };
+    fetchData();
+  }, [casesType]);
+
+  return (
+    <div>
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                pointRadius: 0,
+                data: data,
+                backgroundColor: "rgba(204,16,52,0.5)",
+                borderColor: "#CC1034",
+              },
+            ],
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default LineGraph;
